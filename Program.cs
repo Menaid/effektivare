97d162c9aee63b38293d9f80e5c12b3d8cf33b5f
Menaid <menaidnocic@live.se>
2023-09-22 11:23:10 +0200

    Commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿int[] osorterad = { 5, 9, 7, 25, 1, 78, 65, 42 };
+
+
+int count = 0;
+
+for (int i = 0; i < osorterad.Length; i++)
+{
+    count++;
+    display();
+    sort();
+    
+}
+
+void display()
+{
+    foreach (int a in osorterad)
+    {
+        Console.Write(a + ", ");
+    }
+    Console.WriteLine();
+}
+void sort()
+{
+    for (int i = 0; i < osorterad.Length - 1; i++)
+    {
+        count++;
+        int a = osorterad[i];
+        int b = osorterad[i + 1];
+
+        if (a > b)
+        {
+            osorterad[i] = b;
+            osorterad[i + 1] = a;
+        }
+    }
+}
+
+Console.WriteLine(count);
+
